f11fc3840af6f6ca0965fc244129a212ea4687b6
Козин-Поздняков <vasiliy_pit@mail.ru>
2025-05-06 14:56:44 +0300

    Adding adaptivity, improving the display of select

--- src/components/AccountFormRow.vue
+++ src/components/AccountFormRow.vue
@@ -1,69 +1,66 @@
 <template>
   <el-card shadow="hover" class="account-card">
-    <el-row :gutter="20" align="middle">
-      <el-col :span="6">
-        <el-form-item
-          label="Метки"
-          :error="errors?.labels"
-          label-width="80px"
-          class="form-label"
-        >
-          <el-input
-            v-model="account.labelsInput"
-            :maxlength="50"
-            placeholder="Метки через ;"
-            @blur="onBlur"
-            size="large"
-          />
-        </el-form-item>
-      </el-col>
-      <el-col :span="4">
-        <el-form-item label="Тип записи" label-width="90px" class="form-label">
-          <el-select v-model="account.type" @change="onTypeChange" size="large" style="width: 100%;">
-            <el-option label="Локальная" value="Локальная" />
-            <el-option label="LDAP" value="LDAP" />
-          </el-select>
-        </el-form-item>
-      </el-col>
-      <el-col :span="6">
-        <el-form-item
-          label="Логин"
-          :error="errors?.login"
-          label-width="70px"
-          class="form-label"
-        >
-          <el-input
-            v-model="account.login"
-            :maxlength="100"
-            placeholder="Значение"
-            @blur="onBlur"
-            size="large"
-          />
-        </el-form-item>
-      </el-col>
-      <el-col :span="6" v-if="account.type === 'Локальная'">
-        <el-form-item
-          label="Пароль"
-          :error="errors?.password"
-          label-width="70px"
-          class="form-label"
-        >
-          <el-input
-            v-model="account.password"
-            :maxlength="100"
-            show-password
-            placeholder="Значение"
-            @blur="onBlur"
-            size="large"
-          />
-        </el-form-item>
-      </el-col>
-      <el-col :span="2" style="display: flex; align-items: center; justify-content: center;">
-        <el-button type="danger" @click="$emit('remove')" circle>
-          <el-icon><Close /></el-icon>
-        </el-button>
-      </el-col>
-    </el-row>
+    <div class="account-flex-row">
+      <el-form-item
+        label="Метки"
+        :error="errors?.labels"
+        label-width="80px"
+        class="form-label flex-label"
+      >
+        <el-input
+          v-model="account.labelsInput"
+          :maxlength="50"
+          placeholder="Метки через ;"
+          @blur="onBlur"
+          size="large"
+        />
+      </el-form-item>
+      <el-form-item label="Тип записи" label-width="90px" class="form-label flex-select">
+        <el-select
+          v-model="account.type"
+          @change="onTypeChange"
+          size="large"
+          placeholder="Выберите тип"
+          class="account-type-select"
+          >
+          <el-option label="Локальная" value="Локальная" />
+          <el-option label="LDAP" value="LDAP" />
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        label="Логин"
+        :error="errors?.login"
+        label-width="70px"
+        class="form-label flex-login"
+      >
+        <el-input
+          v-model="account.login"
+          :maxlength="100"
+          placeholder="Значение"
+          @blur="onBlur"
+          size="large"
+        />
+      </el-form-item>
+      <el-form-item
+        v-if="account.type === 'Локальная'"
+        label="Пароль"
+        :error="errors?.password"
+        label-width="70px"
+        class="form-label flex-password"
+      >
+        <el-input
+          v-model="account.password"
+          :maxlength="100"
+          show-password
+          placeholder="Значение"
+          @blur="onBlur"
+          size="large"
+        />
+      </el-form-item>
+      <el-button type="danger" @click="$emit('remove')" circle class="delete-btn">
+        <el-icon><Close /></el-icon>
+      </el-button>
+    </div>
   </el-card>
 </template>
 
@@ -112,22 +109,55 @@ function onBlur() {
   background: #fafcff;
   margin-bottom: 0;
 }
-.form-label .el-form-item__label {
-  font-weight: 600;
-  color: #213547;
-  font-size: 1.05em;
+.account-flex-row {
+  display: flex;
+  align-items: flex-start;
+  gap: 16px;
+  width: 100%;
 }
-.el-input__wrapper {
-  min-width: 180px;
+.flex-label {
+  flex: 1.2 1 180px;
+  min-width: 120px;
+  margin-bottom: 0;
+}
+.flex-select {
+  flex: 1 1 160px;
+  min-width: 140px;
+  margin-bottom: 0;
+}
+.account-type-select {
+  min-width: 120px;
+  width: 100%;
+  font-size: 1.1em;
+}
+.flex-login {
+  flex: 2 2 220px;
+  min-width: 160px;
+  margin-bottom: 0;
+}
+.flex-password {
+  flex: 1.2 1 160px;
+  min-width: 120px;
+  margin-bottom: 0;
+}
+.delete-btn {
+  align-self: center;
+  margin-left: 8px;
 }
 @media (max-width: 900px) {
-  .el-row {
+  .account-flex-row {
     flex-direction: column;
     gap: 12px;
   }
-  .el-col {
-    width: 100% !important;
-    max-width: 100% !important;
+  .flex-label, .flex-select, .flex-login, .flex-password {
+    min-width: 0;
+    width: 100%;
+    flex: none;
+  }
+  .delete-btn {
+    margin-left: 0;
+    width: 48px;
+    align-self: flex-end;
   }
 }
 </style> 
